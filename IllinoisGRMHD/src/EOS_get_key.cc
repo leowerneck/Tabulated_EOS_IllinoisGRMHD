59762f88252012179edd8dfde522d082ad279edb
Leonardo Werneck <leow155@gmail.com>
2021-01-23 14:12:19 -0300

    EOS_get_key.cc: new function to get EOS key at startup

---
+++ IllinoisGRMHD/src/EOS_get_key.cc
@@ -0,0 +1,33 @@
+// Thorn      : IllinoisGRMHD
+// File       : EOS_get_key.cc
+// Author(s)  : Leo Werneck (wernecklr@gmail.com)
+// Description: In this file we set up a function which
+//              retrieves the appropriate EOS key/handle
+//              from the EOS_Omni thorn, setting the
+//              igm_eos_key parameter appropriately.
+
+#include "cctk.h"
+#include "cctk_Arguments.h"
+#include "cctk_Parameters.h"
+
+extern "C" void IllinoisGRMHD_EOS_get_key(CCTK_ARGUMENTS) {
+
+  // This gives us the parameter igm_eos_type
+  DECLARE_CCTK_ARGUMENTS;
+
+  // This gives us the pointer igm_eos_key
+  DECLARE_CCTK_PARAMETERS;
+
+  // Now check what we have
+  if( CCTK_EQUALS(igm_eos_type,"Hybrid") ) {
+    *igm_eos_key = EOS_Omni_GetHandle("Hybrid");
+  }
+  else if( CCTK_EQUALS(igm_eos_type,"Tabulated") || CCTK_EQUALS(igm_eos_type,"nuc_eos") ) {
+    *igm_eos_key = EOS_Omni_GetHandle("nuc_eos");
+  }
+  else {
+    CCTK_VError(__LINE__, __FILE__, CCTK_THORNSTRING,
+                "Unknown EOS type: %s. ABORTING!",igm_eos_type);
+  }
+  
+}
