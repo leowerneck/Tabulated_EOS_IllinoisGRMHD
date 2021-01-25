445d9d26e24501c028375de2df9b6bb9fc12d13a
Leonardo Werneck <leow155@gmail.com>
2021-01-24 23:30:17 -0300

    EOS_headers.hh: added new boolean variables to allow the user to know if we are using Hybrid or Tabulated EOS

--- IllinoisGRMHD/src/EOS_headers.hh
+++ IllinoisGRMHD/src/EOS_headers.hh
@@ -21,6 +21,8 @@ typedef struct _igm_eos_parameters_ {
   //-------------- General parameters --------------
   // EOS key (selects Hybrid or Tabulated EOS)
   CCTK_INT key;
+  // This is useful so we know which EOS type we are using
+  bool is_Hybrid,is_Tabulated;
   // con2prim key (selects the con2prim routine)
   CCTK_INT c2p_routine;
   // Which variable to reconstruct in PPM
