0114006bd013e5ac44a7eda1d7d9943f8bae81cb
Leonardo Werneck <leow155@gmail.com>
2021-01-24 23:30:36 -0300

    EOS_initialize_parameters.cc: added initialization of new boolean variables

--- IllinoisGRMHD/src/EOS_initialize_parameters.cc
+++ IllinoisGRMHD/src/EOS_initialize_parameters.cc
@@ -16,14 +16,17 @@
 void initialize_igm_eos_parameters_from_input( const CCTK_INT* igm_eos_key,const CCTK_REAL cctk_time,igm_eos_parameters &eos ) {
 
   // Set the EOS key
-  // IllinoisGRMHD_EOS_get_key(&eos.key);
-  eos.key         = *igm_eos_key;
-  eos.c2p_routine = Noble2D;
+  eos.key          = *igm_eos_key;
+  eos.c2p_routine  = Noble2D;
+  eos.is_Hybrid    = false;
+  eos.is_Tabulated = false;
 
   if( eos.key == EOS_Omni_GetHandle("Hybrid") ) {
+    eos.is_Hybrid = true;
     initialize_Hybrid_EOS_parameters_from_input(eos);
   }
   else if( eos.key == EOS_Omni_GetHandle("nuc_eos") ) {
+    eos.is_Tabulated = true;
     initialize_Tabulated_EOS_parameters_from_input(cctk_time,eos);
   }
   else {
