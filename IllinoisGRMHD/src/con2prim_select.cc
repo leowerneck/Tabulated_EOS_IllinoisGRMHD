18c5eb300b49b6b6ef97f245e1ed42efb0afcec3
Leonardo Werneck <leow155@gmail.com>
2021-01-25 19:43:15 -0300

    con2prim_select.cc: changed this function to be used by the con2prim wrapper. It now selects (and calls) the appropriate con2prim routine based on the input key

--- IllinoisGRMHD/src/con2prim_select.cc
+++ IllinoisGRMHD/src/con2prim_select.cc
@@ -3,28 +3,50 @@
 
 #include "IllinoisGRMHD_headers.h"
 #include "con2prim_headers.h"
+#include "EOS_headers.hh"
 
-void con2prim_select( CCTK_INT* c2p_key,
-                      int (*con2prim)( const igm_eos_parameters,
-                                       const CCTK_REAL[4][4],const CCTK_REAL[4][4],
-                                       const CCTK_REAL *restrict,CCTK_REAL *restrict ) ) {
+int con2prim_select( const igm_eos_parameters eos,
+                     const CCTK_INT c2p_key,
+                     const CCTK_REAL g4dn[4][4],
+                     const CCTK_REAL g4up[4][4],
+                     const CCTK_REAL *restrict cons,
+                     CCTK_REAL *restrict prim ) {
 
-  DECLARE_CCTK_PARAMETERS;
-  
-  if( CCTK_EQUALS(igm_con2prim_routine,"Noble2D") ) {
-    *c2p_key = Noble2D;
-    con2prim = &con2prim_Noble2D;
-  }
-  else if( CCTK_EQUALS(igm_con2prim_routine,"Palenzuela1D") ) {
-    *c2p_key = Palenzuela1D;
-    con2prim = &con2prim_Palenzuela1D;
-  }
-  else if( CCTK_EQUALS(igm_con2prim_routine,"Palenzuela1D_entropy") ) {
-    *c2p_key = Palenzuela1D_entropy;
-    con2prim = &con2prim_Palenzuela1D_entropy;
-  }
-  else {
-    CCTK_VError(VERR_DEF_PARAMS,"Unknown con2prim method: %s. ABORTING!",igm_con2prim_routine);
-  }
+  switch( c2p_key ) {
 
+    // Modified version of the Palenzuela routine (see https://arxiv.org/pdf/1712.07538.pdf)
+    case Palenzuela1D_entropy:
+      return( con2prim_Palenzuela1D_entropy(eos,g4dn,g4up,cons,prim) );
+      break;
+
+    // Noble2D routine (see https://arxiv.org/pdf/astro-ph/0512420.pdf)
+    case Noble2D:
+      return( con2prim_Noble2D(eos,g4dn,g4up,cons,prim) );
+      break;
+
+    // Noble1D routine (see https://arxiv.org/pdf/astro-ph/0512420.pdf)
+    case Noble1D:
+      return( con2prim_Noble1D(eos,g4dn,g4up,cons,prim) );
+      break;
+
+    // Noble1D entropy routine (see https://arxiv.org/pdf/0808.3140.pdf)
+    case Noble1D_entropy:
+      return( con2prim_Noble1D_entropy(eos,g4dn,g4up,cons,prim) );
+      break;
+
+    // Noble1D entropy 2 routine (see https://arxiv.org/pdf/0808.3140.pdf)
+    case Noble1D_entropy2:
+      return( con2prim_Noble1D_entropy2(eos,g4dn,g4up,cons,prim) );
+      break;
+
+    // Palenzuela 1D routine (see https://arxiv.org/pdf/1712.07538.pdf)
+    case Palenzuela1D:
+      return( con2prim_Palenzuela1D(eos,g4dn,g4up,cons,prim) );
+      break;
+
+    default:
+      CCTK_VError(VERR_DEF_PARAMS,"Unknown c2p key in con2prim_select (%d). ABORTING!",c2p_key);
+      break;
+
+  }
 }
