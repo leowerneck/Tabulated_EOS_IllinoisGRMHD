d0f6fd184da02d10f90bd136415c03646bad868b
Leonardo Werneck <leow155@gmail.com>
2021-01-25 16:14:07 -0300

    driver_evaluate_MHD_rhs.h: updated function arguments

--- IllinoisGRMHD/src/driver_evaluate_MHD_rhs.h
+++ IllinoisGRMHD/src/driver_evaluate_MHD_rhs.h
@@ -2,25 +2,74 @@
 #define DRIVER_EVALUATE_MHD_RHS_H_
 
 /* PRIVATE FUNCTIONS, Called within driver_evaluate_MHD_rhs.C ONLY */
-static void ftilde_gf_compute(const cGH *cctkGH,const int *cctk_lsh,const int flux_dirn,gf_and_gz_struct *input,CCTK_REAL *ftilde_gf);
-static void reconstruct_set_of_prims_PPM(const cGH *cctkGH,const int *cctk_lsh,const int flux_dirn,const int num_prims_to_reconstruct,const int *which_prims_to_reconstruct,
-                                         igm_eos_parameters &eosi,gf_and_gz_struct *in_prims,gf_and_gz_struct *out_prims_r,gf_and_gz_struct *out_prims_l,
-                                         CCTK_REAL *ftilde_gf,CCTK_REAL *temporary);
+static void ftilde_gf_compute( const cGH *restrict cctkGH,
+                               const int *restrict cctk_lsh,
+                               const int flux_dirn,
+                               gf_and_gz_struct *restrict input,
+                               CCTK_REAL *restrict ftilde_gf );
 
-static void compute_tau_rhs_extrinsic_curvature_terms_and_TUPmunu
-(const cGH *cctkGH,const int *cctk_lsh,const int *cctk_nghostzones,CCTK_REAL *dX,CCTK_REAL **metric,gf_and_gz_struct *prims,
- CCTK_REAL **TUPmunu,igm_eos_parameters &eos,
- CCTK_REAL *gupxy,CCTK_REAL *gupxz,CCTK_REAL *gupyz,
- CCTK_REAL *kxx,CCTK_REAL *kxy,CCTK_REAL *kxz,CCTK_REAL *kyy,CCTK_REAL *kyz,CCTK_REAL *kzz,
- CCTK_REAL *tau_rhs);
-static void A_i_rhs_no_gauge_terms(const int A_dirn, const cGH *cctkGH,const int *cctk_lsh,const int *cctk_nghostzones,gf_and_gz_struct *out_prims_r,gf_and_gz_struct *out_prims_l,
-                                   CCTK_REAL *phi_interped,CCTK_REAL *cmax_1,CCTK_REAL *cmin_1,CCTK_REAL *cmax_2,CCTK_REAL *cmin_2, CCTK_REAL *A3_rhs);
+static void reconstruct_set_of_prims_PPM( const igm_eos_parameters eos,
+                                          const cGH *restrict cctkGH,
+                                          const int *restrict cctk_lsh,
+                                          const int flux_dirn,
+                                          const int num_prims_to_reconstruct,
+                                          const int *restrict which_prims_to_reconstruct,
+                                          gf_and_gz_struct *restrict in_prims,
+                                          gf_and_gz_struct *restrict out_prims_r,
+                                          gf_and_gz_struct *restrict out_prims_l,
+                                          CCTK_REAL *restrict ftilde_gf,
+                                          CCTK_REAL *restrict temporary );
 
-static void Lorenz_psi6phi_rhs__add_gauge_terms_to_A_i_rhs(const cGH *cctkGH,const int *cctk_lsh,const int *cctk_nghostzones,CCTK_REAL *dX,CCTK_REAL **interp_vars,CCTK_REAL *psi6phi,
-                                                           CCTK_REAL *shiftx_iphjphkph,CCTK_REAL *shifty_iphjphkph,CCTK_REAL *shiftz_iphjphkph,
-                                                           CCTK_REAL *alpha_iphjphkph,CCTK_REAL *alpha_Phi_minus_betaj_A_j_iphjphkph,CCTK_REAL *alpha_sqrtg_Ax_interp,
-                                                           CCTK_REAL *alpha_sqrtg_Ay_interp,CCTK_REAL *alpha_sqrtg_Az_interp,
-                                                           CCTK_REAL *psi6phi_rhs,CCTK_REAL *Ax_rhs,CCTK_REAL *Ay_rhs,CCTK_REAL *Az_rhs);
+static void compute_tau_rhs_extrinsic_curvature_terms_and_TUPmunu( const igm_eos_parameters eos,
+                                                                   const cGH *restrict cctkGH,
+                                                                   const int *restrict cctk_lsh,
+                                                                   const int *restrict cctk_nghostzones,
+                                                                   CCTK_REAL *restrict dX,
+                                                                   CCTK_REAL **metric,
+                                                                   gf_and_gz_struct *restrict prims,
+                                                                   CCTK_REAL **TUPmunu,
+                                                                   CCTK_REAL *restrict gupxy,
+                                                                   CCTK_REAL *restrict gupxz,
+                                                                   CCTK_REAL *restrict gupyz,
+                                                                   CCTK_REAL *restrict kxx,
+                                                                   CCTK_REAL *restrict kxy,
+                                                                   CCTK_REAL *restrict kxz,
+                                                                   CCTK_REAL *restrict kyy,
+                                                                   CCTK_REAL *restrict kyz,
+                                                                   CCTK_REAL *restrict kzz,
+                                                                   CCTK_REAL *restrict tau_rhs );
+
+static void A_i_rhs_no_gauge_terms( const int A_dirn,
+                                    const cGH *restrict cctkGH,
+                                    const int *restrict cctk_lsh,
+                                    const int *restrict cctk_nghostzones,
+                                    gf_and_gz_struct *restrict out_prims_r,
+                                    gf_and_gz_struct *restrict out_prims_l,
+                                    CCTK_REAL *restrict phi_interped,
+                                    CCTK_REAL *restrict cmax_1,
+                                    CCTK_REAL *restrict cmin_1,
+                                    CCTK_REAL *restrict cmax_2,
+                                    CCTK_REAL *restrict cmin_2,
+                                    CCTK_REAL *restrict A3_rhs );
+
+static void Lorenz_psi6phi_rhs__add_gauge_terms_to_A_i_rhs( const cGH *restrict cctkGH,
+                                                            const int *restrict cctk_lsh,
+                                                            const int *restrict cctk_nghostzones,
+                                                            CCTK_REAL *restrict dX,
+                                                            CCTK_REAL **interp_vars,
+                                                            CCTK_REAL *restrict psi6phi,
+                                                            CCTK_REAL *restrict shiftx_iphjphkph,
+                                                            CCTK_REAL *restrict shifty_iphjphkph,
+                                                            CCTK_REAL *restrict shiftz_iphjphkph,
+                                                            CCTK_REAL *restrict alpha_iphjphkph,
+                                                            CCTK_REAL *restrict alpha_Phi_minus_betaj_A_j_iphjphkph,
+                                                            CCTK_REAL *restrict alpha_sqrtg_Ax_interp,
+                                                            CCTK_REAL *restrict alpha_sqrtg_Ay_interp,
+                                                            CCTK_REAL *restrict alpha_sqrtg_Az_interp,
+                                                            CCTK_REAL *restrict psi6phi_rhs,
+                                                            CCTK_REAL *restrict Ax_rhs,
+                                                            CCTK_REAL *restrict Ay_rhs,
+                                                            CCTK_REAL *restrict Az_rhs );
 
 static void add_fluxes_and_source_terms_to_hydro_rhss( const igm_eos_parameters eos,
                                                        const int flux_dirn,
