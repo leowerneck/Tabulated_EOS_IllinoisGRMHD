fefeac75a9f483bd9411c5d9cf9c9e758ab7c46b
Leonardo Werneck <leow155@gmail.com>
2021-01-27 20:02:45 -0300

    nuc_eos_short.cc: added new table inversion routine, which finds the temperature from the pressure

--- EOS_Omni/src/nuc_eos_cxx/nuc_eos_short.cc
+++ EOS_Omni/src/nuc_eos_cxx/nuc_eos_short.cc
@@ -320,4 +320,115 @@ extern "C"
     return;
   }
 
+  extern "C"
+  void CCTK_FNAME(nuc_eos_m_kt3_short)(const int *restrict n_in,
+                                       const double *restrict rho, 
+                                       double *restrict temp,
+                                       const double *restrict ye,
+                                       double *restrict eps,
+                                       const double *restrict prs,
+                                       double *restrict ent,
+                                       double *restrict cs2,
+                                       double *restrict dedt,
+                                       double *restrict dpderho,
+                                       double *restrict dpdrhoe,
+                                       double *restrict munu,
+                                       const double *restrict prec,
+                                       int *restrict keyerr,
+                                       int *restrict anyerr)
+{
+
+  using namespace nuc_eos;
+
+  const int n = *n_in;
+
+  *anyerr = 0;
+
+  for(int i=0;i<n;i++) {
+    
+    // check if we are fine
+    // Note that this code now requires that the
+    // temperature guess be within the table bounds
+    keyerr[i] = checkbounds_kt0_noTcheck(rho[i], ye[i]);
+    if(CCTK_BUILTIN_EXPECT(keyerr[i] != 0, false)) {
+      *anyerr = 1;
+    }
+  }
+
+  // Abort if there is any error in checkbounds.
+  // This should never happen and the program should abort with
+  // a fatal error anyway. No point in doing any further EOS calculations.
+  if(*anyerr) return;
+
+  for(int i=0;i<n;i++) {
+    const double lr = log(rho[i]);
+    const double lt = log(MIN(MAX(temp[i],eos_tempmin),eos_tempmax));
+    double ltout;
+
+    const double lxprs = log(prs[i]);
+#if DEBUG
+      fprintf(stderr,"%d %15.6E %15.6E %15.6E %15.6E\n",i,lr,lt,ye[i],lxprs);
+      fprintf(stderr,"%d %15.6E %15.6E %15.6E %15.6E\n",i,
+	      exp(lr),exp(lt),ye[i],exp(lxprs));
+#endif
+
+      nuc_eos_findtemp_press(lr,lt,ye[i],lxprs,*prec,
+                             (double *restrict)(&ltout),&keyerr[i]);
+
+
+    if(CCTK_BUILTIN_EXPECT(keyerr[i] != 0, false)) {
+      *anyerr = 1;
+    } else {
+      temp[i] = exp(ltout);
+      int idx[8];
+      double delx,dely,delz;
+      get_interp_spots(lr,ltout,ye[i],&delx,&dely,&delz,idx);
+      // get eps
+      {
+	const int iv = 1;
+	nuc_eos_C_linterp_one(idx,delx,dely,delz,&(eps[i]),iv);
+      }
+      // get entropy
+      {
+	const int iv = 2;
+	nuc_eos_C_linterp_one(idx,delx,dely,delz,&(ent[i]),iv);
+      }
+      // get munu
+      {
+	const int iv = 3;
+	nuc_eos_C_linterp_one(idx,delx,dely,delz,&(munu[i]),iv);
+      }
+      // get cs2
+      {
+	const int iv = 4;
+	nuc_eos_C_linterp_one(idx,delx,dely,delz,&(cs2[i]),iv);
+      }
+      // get dedT
+      {
+	const int iv = 5;
+	nuc_eos_C_linterp_one(idx,delx,dely,delz,&(dedt[i]),iv);
+      }
+      // get dpdrhoe
+      {
+	const int iv = 6;
+	nuc_eos_C_linterp_one(idx,delx,dely,delz,&(dpdrhoe[i]),iv);
+      }
+      // get dpderho
+      {
+	const int iv = 7;
+	nuc_eos_C_linterp_one(idx,delx,dely,delz,&(dpderho[i]),iv);
+      }
+    }
+  }
+
+  for(int i=0;i<n;i++) {
+    eps[i] = exp(eps[i]) - energy_shift;
+#if HAVEGR
+    cs2[i] = rho[i] * cs2[i] / (rho[i] + rho[i] * eps[i] + prs[i]);
+#endif
+  }
+
+  return;
+}
+
 } // namespace nuc_eos
