97da1cfac1170571d919bebac2b1afc672c641a9
Leonardo Werneck <leow155@gmail.com>
2021-02-03 17:40:00 -0300

    con2prim_select.cc: updated function calls to reflect the fact that the eos struct is now a pointer. removed Palenzuela_entropy

--- IllinoisGRMHD/src/con2prim_select.cc
+++ IllinoisGRMHD/src/con2prim_select.cc
@@ -5,7 +5,7 @@
 #include "con2prim_headers.h"
 #include "EOS_headers.hh"
 
-int con2prim_select( const igm_eos_parameters eos,
+int con2prim_select( igm_eos_parameters *restrict eos,
                      const CCTK_INT c2p_key,
                      const CCTK_REAL *restrict adm_quantities,
                      const CCTK_REAL g4dn[4][4],
@@ -17,32 +17,27 @@ int con2prim_select( const igm_eos_parameters eos,
 
     // Noble2D routine (see https://arxiv.org/pdf/astro-ph/0512420.pdf)
     case Noble2D:
-      return( con2prim_Noble2D(eos,g4dn,g4up,cons,prim) );
+      return( con2prim_Noble2D(*eos,g4dn,g4up,cons,prim) );
       break;
 
     // Noble1D routine (see https://arxiv.org/pdf/astro-ph/0512420.pdf)
     case Noble1D:
-      return( con2prim_Noble1D(eos,g4dn,g4up,cons,prim) );
+      return( con2prim_Noble1D(*eos,g4dn,g4up,cons,prim) );
       break;
 
     // Noble1D entropy routine (see https://arxiv.org/pdf/0808.3140.pdf)
     case Noble1D_entropy:
-      return( con2prim_Noble1D_entropy(eos,g4dn,g4up,cons,prim) );
+      return( con2prim_Noble1D_entropy(*eos,g4dn,g4up,cons,prim) );
       break;
 
     // Noble1D entropy 2 routine (see https://arxiv.org/pdf/0808.3140.pdf)
     case Noble1D_entropy2:
-      return( con2prim_Noble1D_entropy2(eos,g4dn,g4up,cons,prim) );
+      return( con2prim_Noble1D_entropy2(*eos,g4dn,g4up,cons,prim) );
       break;
 
     // Cerda-Duran et al. 2D routine (see https://arxiv.org/pdf/0804.4572.pdf)
     case CerdaDuran2D:
-      return( con2prim_CerdaDuran2D(eos,adm_quantities,cons,prim) );
-
-    // Modified version of the Palenzuela routine (see https://arxiv.org/pdf/1712.07538.pdf)
-    case Palenzuela1D_entropy:
-      return( con2prim_Palenzuela1D_entropy(eos,adm_quantities,cons,prim) );
-      break;
+      return( con2prim_CerdaDuran2D(*eos,adm_quantities,cons,prim) );
 
     // Palenzuela 1D routine (see https://arxiv.org/pdf/1712.07538.pdf)
     case Palenzuela1D:
