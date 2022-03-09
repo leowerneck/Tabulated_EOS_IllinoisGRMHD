b022ced52c2f67cd43cbf739143dd629606b775e
Leo Werneck <leow155@gmail.com>
2022-03-09 10:17:42 -0800

    NRPyLeakageET/src/NRPyLeakageET_optical_depths_initialization_routines.c: removed unused function

--- NRPyLeakageET/src/NRPyLeakageET_optical_depths_initialization_routines.c
+++ NRPyLeakageET/src/NRPyLeakageET_optical_depths_initialization_routines.c
@@ -4,24 +4,6 @@
 
 #include "NRPyLeakageET.h"
 
-void NRPyLeakageET_GetMaxSize(CCTK_ARGUMENTS,int *IterationCounter) {
-  DECLARE_CCTK_ARGUMENTS;
-  DECLARE_CCTK_PARAMETERS;
-
-  if(!NRPyLeakageET_ProcessOwnsData()) return;
-
-  const int IterationCounter_in = *IterationCounter;
-
-  *IterationCounter = MAX(IterationCounter_in,MAX(cctk_lsh[0],MAX(cctk_lsh[1],cctk_lsh[2])));
-
-  if(verbosity_level>1) {
-    CCTK_VInfo(CCTK_THORNSTRING,"Inside NRPyLeakageET_GetMaxSize");
-    CCTK_VInfo(CCTK_THORNSTRING,"Input counter : %d",*IterationCounter);
-    CCTK_VInfo(CCTK_THORNSTRING,"Local shape   : %d %d %d",cctk_lsh[0],cctk_lsh[1],cctk_lsh[2]);
-    CCTK_VInfo(CCTK_THORNSTRING,"Output counter: %d",*IterationCounter);
-  }
-}
-
 void NRPyLeakageET_optical_depths_initialize_to_zero(CCTK_ARGUMENTS) {
   DECLARE_CCTK_ARGUMENTS;
   DECLARE_CCTK_PARAMETERS;
