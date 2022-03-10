c435bb0d2f33452b5eaf6b6d04880867b0b96c92
Leo Werneck <leow155@gmail.com>
2022-03-09 18:54:34 -0800

    NRPyLeakageET_compute_opacities.c: added rho_threshold check

--- NRPyLeakageET/src/NRPyLeakageET_compute_opacities.c
+++ NRPyLeakageET/src/NRPyLeakageET_compute_opacities.c
@@ -23,29 +23,51 @@ void NRPyLeakageET_compute_opacities(CCTK_ARGUMENTS) {
     for(int k=0;k<cctk_lsh[2];k++) {
       for(int j=0;j<cctk_lsh[1];j++) {
         for(int i=0;i<cctk_lsh[0];i++) {
-          // Step 1: Read from main memory
-          const CCTK_INT index         = CCTK_GFINDEX3D(cctkGH,i,j,k);
-          const CCTK_REAL rhoL         = rho[index];
-          const CCTK_REAL Y_eL         = Y_e[index];
-          const CCTK_REAL temperatureL = temperature[index];
-          const CCTK_REAL tau_nue [2]  = {tau_0_nue [index],tau_1_nue [index]};
-          const CCTK_REAL tau_anue[2]  = {tau_0_anue[index],tau_1_anue[index]};
-          const CCTK_REAL tau_nux [2]  = {tau_0_nux [index],tau_1_nux [index]};
+          // Step 1: Set local gridfunction index
+          const CCTK_INT index = CCTK_GFINDEX3D(cctkGH,i,j,k);
 
-          // Step 2: Compute opacities
+          // Step 2: Declare variables, read in density
+          const CCTK_REAL rhoL = rho[index];
           CCTK_REAL kappa_nue[2], kappa_anue[2], kappa_nux[2];
-          NRPyLeakageET_compute_opacities_nrpy_constants(rhoL,Y_eL,temperatureL,
-                                                         tau_nue,tau_anue,tau_nux,
-                                                         kappa_nue,kappa_anue,kappa_nux);
+          CCTK_REAL tau_nue[2], tau_anue[2], tau_nux[2];
 
-          // Step 3: Write to main memory
+          // Step 3: Check density threshold, compute opacities
+          if( rhoL < rho_threshold ) {
+            // Step 3.a: Below density threshold; set opacities and optical
+            // depths to zero
+            kappa_nue[0] = kappa_anue[0] = kappa_nux[0] = 0.0;
+            kappa_nue[1] = kappa_anue[1] = kappa_nux[1] = 0.0;
+            tau_nue  [0] = tau_anue  [0] = tau_nux  [0] = 0.0;
+            tau_nue  [1] = tau_anue  [1] = tau_nux  [1] = 0.0;
+            CCTK_VINFO("%s BAAAAAAD",__func__);
+          }
+          else {
+            // Step 3.b: Above density threshold, compute opacities
+            // Step 3.b.i: Read from main memory
+            const CCTK_REAL Y_eL         = Y_e[index];
+            const CCTK_REAL temperatureL = temperature[index];
+            tau_nue [0]                  = tau_0_nue [index];
+            tau_nue [1]                  = tau_1_nue [index];
+            tau_anue[0]                  = tau_0_anue[index];
+            tau_anue[1]                  = tau_1_anue[index];
+            tau_nux [0]                  = tau_0_nux [index];
+            tau_nux [1]                  = tau_1_nux [index];
+
+            // Step 3.b.ii: Compute opacities
+            NRPyLeakageET_compute_opacities_nrpy_constants(rhoL,Y_eL,temperatureL,
+                                                           tau_nue,tau_anue,tau_nux,
+                                                           kappa_nue,kappa_anue,kappa_nux);
+          }
+
+          // Step 4: Write to main memory
           kappa_0_nue [index] = kappa_nue [0];
           kappa_1_nue [index] = kappa_nue [1];
           kappa_0_anue[index] = kappa_anue[0];
           kappa_1_anue[index] = kappa_anue[1];
           kappa_0_nux [index] = kappa_nux [0];
           kappa_1_nux [index] = kappa_nux [1];
-          // We copy the optical depths to the previous time level as a placeholder
+
+          // Step 5: Copy the optical depths to the previous time level as a placeholder
           tau_0_nue_p [index] = tau_nue [0];
           tau_1_nue_p [index] = tau_nue [1];
           tau_0_anue_p[index] = tau_anue[0];
@@ -61,28 +83,56 @@ void NRPyLeakageET_compute_opacities(CCTK_ARGUMENTS) {
     for(int k=0;k<cctk_lsh[2];k++) {
       for(int j=0;j<cctk_lsh[1];j++) {
         for(int i=0;i<cctk_lsh[0];i++) {
-          // Step 1: Read from main memory
-          const CCTK_INT index         = CCTK_GFINDEX3D(cctkGH,i,j,k);
-          const CCTK_REAL rhoL         = rho[index];
-          const CCTK_REAL Y_eL         = Y_e[index];
-          const CCTK_REAL temperatureL = temperature[index];
-          const CCTK_REAL tau_nue [2]  = {tau_0_nue [index],tau_1_nue [index]};
-          const CCTK_REAL tau_anue[2]  = {tau_0_anue[index],tau_1_anue[index]};
-          const CCTK_REAL tau_nux [2]  = {tau_0_nux [index],tau_1_nux [index]};
+          // Step 1: Set local gridfunction index
+          const CCTK_INT index = CCTK_GFINDEX3D(cctkGH,i,j,k);
 
-          // Step 2: Compute opacities
+          // Step 2: Declare variables, read in density
+          const CCTK_REAL rhoL = rho[index];
           CCTK_REAL kappa_nue[2], kappa_anue[2], kappa_nux[2];
-          NRPyLeakageET_compute_opacities_harm_constants(rhoL,Y_eL,temperatureL,
-                                                         tau_nue,tau_anue,tau_nux,
-                                                         kappa_nue,kappa_anue,kappa_nux);
+          CCTK_REAL tau_nue[2], tau_anue[2], tau_nux[2];
 
-          // Step 3: Write to main memory
+          // Step 3: Check density threshold, compute opacities
+          if( rhoL < rho_threshold ) {
+            // Step 3.a: Below density threshold; set opacities and optical
+            // depths to zero
+            kappa_nue[0] = kappa_anue[0] = kappa_nux[0] = 0.0;
+            kappa_nue[1] = kappa_anue[1] = kappa_nux[1] = 0.0;
+            tau_nue  [0] = tau_anue  [0] = tau_nux  [0] = 0.0;
+            tau_nue  [1] = tau_anue  [1] = tau_nux  [1] = 0.0;
+          }
+          else {
+            // Step 3.b: Above density threshold, compute opacities
+            // Step 3.b.i: Read from main memory
+            const CCTK_REAL Y_eL         = Y_e[index];
+            const CCTK_REAL temperatureL = temperature[index];
+            tau_nue [0]                  = tau_0_nue [index];
+            tau_nue [1]                  = tau_1_nue [index];
+            tau_anue[0]                  = tau_0_anue[index];
+            tau_anue[1]                  = tau_1_anue[index];
+            tau_nux [0]                  = tau_0_nux [index];
+            tau_nux [1]                  = tau_1_nux [index];
+
+            // Step 3.b.ii: Compute opacities
+            NRPyLeakageET_compute_opacities_harm_constants(rhoL,Y_eL,temperatureL,
+                                                           tau_nue,tau_anue,tau_nux,
+                                                           kappa_nue,kappa_anue,kappa_nux);
+          }
+
+          // Step 4: Write to main memory
           kappa_0_nue [index] = kappa_nue [0];
           kappa_1_nue [index] = kappa_nue [1];
           kappa_0_anue[index] = kappa_anue[0];
           kappa_1_anue[index] = kappa_anue[1];
           kappa_0_nux [index] = kappa_nux [0];
           kappa_1_nux [index] = kappa_nux [1];
+
+          // Step 5: Copy the optical depths to the previous time level as a placeholder
+          tau_0_nue_p [index] = tau_nue [0];
+          tau_1_nue_p [index] = tau_nue [1];
+          tau_0_anue_p[index] = tau_anue[0];
+          tau_1_anue_p[index] = tau_anue[1];
+          tau_0_nux_p [index] = tau_nux [0];
+          tau_1_nux_p [index] = tau_nux [1];
         }
       }
     }
